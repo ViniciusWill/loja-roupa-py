36042445908df9e82aaf94482531794954e660e3
Vinicius William Gomes Souza <vinicius.gomes@GFP.LOCAL>
2026-02-02 17:12:53 -0300

    2026-02-02

--- loja_python.py
+++ loja_python.py
@@ -1,10 +1,10 @@
 
 import pandas as pd
 import os as os
-from datetime import datetime as data
+
 
 nomearq = "Compras.xlsx"
-Hoje = data.now()
+
 #LerArquivo---------------------------------------------
 def ler():
     if os.path.exists(nomearq):
@@ -15,41 +15,17 @@ def ler():
         return False
 #LerCaixaDiario---------------------------------------
 def Caixa():
-     try:
+    try:
         print('Verificando caixa diario...')
         caixa = pd.read_excel(nomearq, sheet_name='Caixa')
+        caixa = caixa.sort_values("Data")
         caixa.columns = caixa.columns.str.strip()
         return caixa
-     except:
-        print('Caixa não encontrado')
 
-#LerComprasDiarias------------------------------------------ 
-def Comprasdiarias():
-    try:
-        caixa = Caixa()
-        if caixa is None:
-         print("Caixa inválido")
-         return
-        comprasday =  caixa[caixa["Operação"] == "Compra"]
-        print('Compras diarias encontradas.')
-        return comprasday
     except Exception as e:
-        print('Compras diarias não encontradas')
-        print("Erro", e)
-
-#LerVendasDiaria--------------------------------------------
-def Vendasdiarias():
-     try:    
-        caixa = Caixa()
-        if caixa is None:
-            print("Caixa invalido")
-            return
-        vendasday = caixa[caixa["Operação"] == "Venda"]
-        print('Vendas diarias encontradas.')
-        return vendasday
-     except Exception as e:
-        print('Vendas diarias não encontrads')
-        print("Erro", e)
+        print('Caixa não encontrado')
+        print(e)
+        return pd.DataFrame()
 #LerEstoque-------------------------------------------------
 def Estoque():
      try:
@@ -60,74 +36,106 @@ def Estoque():
      except:
          print('Estoque não encontrado')
 
-#LançarComprasDiarias----------------------------------------
-def lancarcompdia():
+#LerComprasEvendasTotal---------------------------------------
+def comptotal():
+    try:
+        compras = pd.read_excel(nomearq, sheet_name="Compras")
+        compras.columns = compras.columns.str.strip()
+        print('Compras Totais encontradas')
+        return compras
+    except Exception as e:
+        print('Compras Totais não encontradas!')
+        print(f'Erro: {e}')
+
+
+def vendtotal():
+    try:
+        vendas = pd.read_excel(nomearq, sheet_name="Vendas")
+        vendas.columns = vendas.columns.str.strip()
+        print('Vendas totais encontradas')
+        return vendas
+    except Exception as e:
+        print('Vendas totais não encontradas')
+        print(f'Erro: {e}')
+
+#LançarOperacoesDiarias----------------------------------------
+def lancaropdia():
+    caixa = Caixa()
     estoque = Estoque()
-    comprasdias = Comprasdiarias()
-    print(comprasdias)
+    comprast = comptotal()
+    vendast = vendtotal()
     print('Verificando produtos que estão no estoque..')
-    for _,linha in comprasdias.iterrows():
+    for _,linha in caixa.iterrows():
         produto = linha["Nome do produto"] 
         Tamanho = linha["Tamanho"] 
-        qntcomp = linha["Quantidade"]
-    
-        condicaocomp = ( (estoque["Nome do produto"] == produto) & 
+        qnt = linha["Quantidade"]
+        valoruni = linha["Valor unitario compra"]
+        valorven = linha["Valor unitario venda"]
+        operacao = linha["Operação"]
+        data = linha["Data"]
+        sexo = linha["Sexo"]
+
+        condicao = ( (estoque["Nome do produto"] == produto) & 
                     (estoque["Tamanho"] == Tamanho))
-        if condicaocomp.any():
-            qntatt = estoque.loc[condicaocomp, "Quantidade"].iloc[0] + qntcomp
-            print(f"Produto encontrado: {produto} - {Tamanho}")
-            print(f"Produto no estoque após compra: {produto} - {Tamanho} - Unidades: {qntatt}")
-
+        if operacao == "Compra":
+            if condicao.any():
+                qntattest = estoque.loc[condicao, "Quantidade"].iloc[0] + qnt
+                estoque.loc[condicao, "Quantidade"] = qntattest
+                print(f"Produto encontrado: {produto} - {Tamanho} - {qnt}")
+                print(f"Produto no estoque após compra: {produto} - {Tamanho} - Unidades: {qntattest}")
+                novacomp = pd.DataFrame([{"Nome do produto": produto, 
+                                        "Tamanho": Tamanho,
+                                        "Quantidade": qnt,
+                                        "Valor unitario compra": valoruni,
+                                        "Data da compra": data,
+                                        "Sexo": sexo }])
+                comprast = pd.concat([comprast, novacomp], ignore_index=True)
+            else:
+                print(f"Produto não encontrado: {produto} - {Tamanho}")
+                print("Cadastrando..")
+                novoprod = pd.DataFrame([{"Nome do produto": produto, 
+                                           "Tamanho": Tamanho,
+                                            "Quantidade": qnt, 
+                                            "Valor unitario compra": valoruni
+                                             }])
+                novacompT = pd.DataFrame([{"Nome do produto": produto, 
+                                           "Tamanho": Tamanho,
+                                            "Quantidade": qnt, 
+                                            "Valor unitario compra": valoruni,
+                                            "Data da compra": data,
+                                            "Sexo": sexo
+                                             }])
+                estoque = pd.concat([estoque, novoprod], ignore_index=True)
+                novacomp = pd.conca([comprast, novacompT], ignore_index=True)
+                
+        elif operacao == "Venda":
+            if condicao.any():
+                qntattest = estoque.loc[condicao, "Quantidade"].iloc[0] - qnt
+                if qntattest >= 0:
+                     estoque.loc[condicao, "Quantidade"] = qntattest
+                     print(f"Produto encontrado: {produto} - {Tamanho}")
+                     print(f"Produto no estoque após venda: {produto} - {Tamanho} - {qntattest}")
+                     novavendaT = pd.DataFrame([{"Nome do produto": produto,
+                                           "Tamanho": Tamanho,
+                                           "Quantidade": qnt,
+                                           "Valor unitario venda": valorven,
+                                            "Data": data,
+                                             "Sexo": sexo }])
+                     novavenda = pd.concat([vendast,novavendaT], ignore_index=True)
+                else:
+                    print('Não há unidades do produto disponiveis para venda!')
+            else:
+                print(f"Produto não cadastrado no estoque! {produto} - {Tamanho}")
+    
         else:
-            print(f"Produto não encontrado: {produto} - {Tamanho}")
+              print(f"Foi encontrado uma operação invalida, verifique as operações lançadas no caixa! Operação: {operacao}!")
 
-#LançarVendasDiarias-------------------------------------------
-def lancarvendia():
-    estoque = Estoque()
-    vendia = Vendasdiarias()
-    print(vendia)
-    print('Verificando estoque dos produtos vendidos..')
-    for _,linha in vendia.iterrows():
-        produto = linha["Nome do produto"]
-        Tamanho = linha["Tamanho"]
-        qntdvend = linha["Quantidade"]
-        condicaovenda = ( (estoque["Nome do produto"] == produto) &
-                           (estoque["Tamanho"] == Tamanho))
-        
-        if condicaovenda.any(): 
-            qntatt = estoque.loc[condicaovenda, "Quantidade"].iloc[0] - qntdvend
-            print(f"Produto encontrado no estoque: {produto} - {Tamanho}")
-            print(f"Produto no estoque após venda: {produto} - {Tamanho} - Unidades: {qntatt} ") 
-        else:
-            print(f"Atenção! O produto vendido não está cadastrado no estoque! {produto} - {Tamanho}")
-
-
-
-#ComprasTotal-------------------------------------------
-def comprascalc():
-    try:
-        comp = pd.read_excel(nomearq, sheet_name="Compras")
-        comp.columns = comp.columns.str.strip()
-        print('Compra total encontrada')
-        return comp
-    except:
-        print('Compras não encontradas')
-#VendasTotal---------------------------------------------------
-def vendascalc():
-   try:
-     vend = pd.read_excel(nomearq, sheet_name='Vendas')
-     vend.columns = vend.columns.str.strip()
-     print(vend)
-     return vend
-   except:
-     print('Vendas não encontradas')
-
+        with pd.ExcelWriter(nomearq, mode="a", if_sheet_exists='replace', engine="openpyxl") as writer:
+                    estoque.to_excel(writer, sheet_name="Estoque", index=False)
+                    comprast.to_excel(writer, sheet_name="Compras", index=False)
 #Principal--------------------------------------------
 print("Buscando Arquivo..")
 Leitura = ler()
-
 if Leitura == True:
-    Comprasdiarias()
-    lancarcompdia()
-    lancarvendia()
-    
+    lancaropdia()
+   
